b554a8f0b4ee120ea1af8106ccda8408a686c387
Gabriel Torres <elcomparosh97@gmail.com>
2024-02-08 17:37:19 -0600

    Agregar archivos de proyecto.

---
+++ Area_triangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿double b, h, area;
+
+Console.WriteLine("Enter the base of your triangle in cm:");
+b=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the height of your triangle in cm:");
+h = Convert.ToDouble(Console.ReadLine());
+
+area = b * h;
+
+Console.WriteLine("The area of your triangle is: " + area+ "cm²");
+Console.ReadKey();
